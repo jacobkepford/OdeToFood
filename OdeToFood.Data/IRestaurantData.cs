e4b659227dfd1ecb8808efc7c92acb9a58964597
Jacob <jacobkepford@gmail.com>
2021-05-18 20:40:35 -0500

    updating git ignore

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,13 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetById(int id);
+    }
+}
