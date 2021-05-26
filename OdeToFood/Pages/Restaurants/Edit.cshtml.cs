8431cf43417878a1855445bc835b8cc95f0d7104
Jacob <jacobkepford@gmail.com>
2021-05-25 21:04:52 -0500

    populating cuisine select on get post as well now

--- OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -38,7 +38,8 @@ namespace OdeToFood.Pages.Restaurants
 
         public IActionResult OnPost()
         {
-            Restaurant = restaurantData.Update(Restaurant);
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+            restaurantData.Update(Restaurant);
             restaurantData.Commit();
             return Page();
         }
