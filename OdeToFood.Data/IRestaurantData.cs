90ad4a350cef8e8d0b2b01c62c52adcfada5ad61
Jacob <jacobkepford@gmail.com>
2021-05-29 11:07:13 -0500

    pulled latest and merged from server

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -10,12 +10,9 @@ namespace OdeToFood.Data
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
         Restaurant GetById(int id);
         Restaurant Update(Restaurant updatedRestaurant);
-<<<<<<< HEAD
-        Restaurant Delete(int id);
-=======
         Restaurant Add(Restaurant newRestaurant);
->>>>>>> 632598f3ca5b33ba2ab9e989426133dd6c0d031b
         int Commit();
+        Restaurant Delete(int id);
 
     }
 }
